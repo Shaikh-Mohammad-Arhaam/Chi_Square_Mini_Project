afbab212a62b174fc821bb2af29425294de7e368
Shaikh-Mohammad-Arhaam <your_email@example.com>
2026-03-26 19:24:15 +0530

    Few comments changes

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import pandas as pd
 from chi_square.manual import chi_square_manual
 from chi_square.scipy_test import chi_square_scipy
 
-# Load CSV data
+#Load CSV data
 data = pd.read_csv('data/rental_data.csv')
 
 print("Dataset:\n", data)
-# Extract observed frequency matrix from the dataframe
+#  Extract observed frequency matrix from the dataframe
 observed = data[['Booked', 'NotBooked']].values
 
 print("\nObserved Data:\n", observed)
-# Run manual chi-square calculation for comparison
+#  Run manual chi-square calculation for comparison
 
 chi_manual = chi_square_manual(observed)
 print("\nManual Chi-Square:", chi_manual)
